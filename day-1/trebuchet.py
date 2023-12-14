76b4e7ce75f458b3abbdd1b19987d2739379103f
Apophany <bagnallcf1@gmail.com>
2023-12-14 20:46:27 +0000

    Day 1 - Part 1 - revision

--- day-1/trebuchet.py
+++ day-1/trebuchet.py
@@ -1,26 +1,31 @@
 def calculate_sum(lines):
     sum = 0
     for line in lines:
-        first = None
-        second = None
         line = line.replace("\n", "")
-
-        for char in line:
-            int_char = to_int(char)
-            if int_char == 0:
-                continue
-
-            if first is None:
-                first = char
-                second = char
-            else:
-                second = char
-
-        char_num = first + second
-        sum += to_int(char_num)
-        print(f"Line is: {line}. Number is: {char_num}, Sum is: {sum}")
+        val_for_line = value_for_line(line)
+        sum += val_for_line
+        print(f"Line is: {line}. Number is: {val_for_line}, Sum is: {sum}")
 
     return sum
+
+
+def value_for_line(line):
+    first = None
+    second = None
+    line = line.replace("\n", "")
+
+    for char in line:
+        int_char = to_int(char)
+        if int_char == 0:
+            continue
+
+        if first is None:
+            first = char
+            second = char
+        else:
+            second = char
+
+    return to_int(first + second)
 
 
 def to_int(str_int):
@@ -34,4 +39,6 @@ if __name__ == "__main__":
     with open("./resources/input.txt", "r") as input_file:
         lines = input_file.readlines()
 
-    print(calculate_sum(lines))
+    sum = calculate_sum(lines)
+    print(sum)
+    assert sum == 54927
