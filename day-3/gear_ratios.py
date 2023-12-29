69bfb6452399bc80a4c844b19e9a830d4eb29120
Apophany <bagnallcf1@gmail.com>
2023-12-29 21:43:12 +0000

    Initial go at day 3 pt 1

--- day-3/gear_ratios.py
+++ day-3/gear_ratios.py
@@ -1,4 +1,98 @@
 from util import reader
 
+valid_symbols = "!@#$%^&*()_-+={}[]"
+
+
+def create_matrix(engine_schematic):
+    matrix = []
+    for line in engine_schematic:
+        matrix.append(list(line))
+    return matrix
+
+
+def sum_part_numbers(engine_matrix):
+    num_start_index = -1
+    num_end_index = -1
+
+    sum_parts = 0
+
+    for y, engine_y in enumerate(engine_matrix):
+        for x, engine_x in enumerate(engine_y):
+            if engine_x.isnumeric():
+                if num_start_index == -1:
+                    num_start_index = x
+                else:
+                    num_end_index = x
+            else:
+                sum_parts += calc_part_value(engine_matrix, num_start_index, num_end_index, y)
+                num_start_index = -1
+                num_end_index = -1
+
+        sum_parts += calc_part_value(engine_matrix, num_start_index, num_end_index, y)
+
+    return sum_parts
+
+
+def within_bounds(engine_matrix, y, x):
+    if 0 <= y < len(engine_matrix):
+        if 0 <= x < len(engine_matrix[y]):
+            return True
+
+    return False
+
+
+def is_symbol_adjacent(engine_matrix, y, x):
+    offsets = [-1, 0, 1]
+    for y_offset in offsets:
+        for x_offset in offsets:
+            curr_y = y + y_offset
+            curr_x = x + x_offset
+
+            if within_bounds(engine_matrix, curr_y, curr_x):
+                if engine_matrix[curr_y][curr_x] in valid_symbols:
+                    return True
+
+    return False
+
+
+def is_adjacent(engine_matrix, start_coords, end_coords):
+    adjacent = False
+    start_x = start_coords[1]
+    end_x = end_coords[1]
+
+    curr_x = start_x
+    while curr_x <= end_x:
+        if is_symbol_adjacent(engine_matrix, start_coords[0], curr_x):
+            adjacent = True
+            break
+        curr_x += 1
+
+    return adjacent
+
+
+def convert_to_int(engine_matrix, start_coords, end_coords):
+    start_x = start_coords[1]
+    end_x = end_coords[1]
+
+    curr_x = start_x
+    str_int = ""
+    while curr_x <= end_x:
+        str_int += engine_matrix[start_coords[0]][curr_x]
+        curr_x += 1
+
+    return int(str_int)
+
+
+def calc_part_value(engine_matrix, num_start_index, num_end_index, y):
+    part_value = 0
+    if not num_start_index == -1 and not num_end_index == -1:
+        if is_adjacent(engine_matrix, [y, num_start_index], [y, num_end_index]):
+            part_value = convert_to_int(engine_matrix, [y, num_start_index], [y, num_end_index])
+
+    return part_value
+
+
 if __name__ == "__main__":
-    input = reader.read("resources/input.txt")
+    puzzle_input = reader.read("resources/input.txt")
+    part_numbers = sum_part_numbers(create_matrix(puzzle_input))
+    assert part_numbers == 531167
